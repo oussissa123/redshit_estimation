4b36e825e7e20176b98e63e51fd4e341aba55f5b
Ousmane Issa <issaousmane7@gmail.com>
2018-04-26 18:45:40 +0200

    ajout de quelques fichier

--- new/utils.py
+++ new/utils.py
@@ -10,6 +10,8 @@ from keras.models import model_from_json
 from sklearn.model_selection import train_test_split
 from IPython.display import SVG
 import pylab as pl
+import glob as g
+
 
 bands = ['u', 'g', 'r', 'i', 'z']
 
@@ -49,25 +51,7 @@ def comp_dowload(rerun, run, camcol, field, file):
     saveFile = open(file,'wb')
     saveFile.write(dowload_as_fitbz2(form_url(rerun, run, camcol, field)))
     saveFile.close()
-
-"""
-def fit_dowload(rerun, run, camcol, field, file):
-    values = ['u', 'g', 'r', 'i', 'z']
-    pos =0;
-    for url in form_url(rerun, run, camcol, field):
-        saveFile = open(file+'-'+values[pos]+'.fits','wb')
-        saveFile.write(dowload_as_fit(url))
-        saveFile.close()
-        pos = 1 + pos
-
-def fit_dowload(rerun, run, camcol, field, directory):
-    values = ['u', 'g', 'r', 'i', 'z']
-    pos =0;
-    for url in form_url(rerun, run, camcol, field):
-        saveFile = open(directory+'/'+rerun+'-'+run+'-'+camcol+'-'+field+'-'+values[pos]+'.fits','wb')
-        saveFile.write(dowload_as_fit(url))
-        saveFile.close()
-        pos = 1 + pos """ 
+ 
           
         
 def getName(rerun, run, camcol, field, filtre):
@@ -84,105 +68,38 @@ def getName3(rerun, run, camcol, field):
 def createDirectory(rep):
     if not os.path.isdir(rep):
         os.makedirs(rep)
-"""
-def allurlinseries(file):
-    objects = pd.read_csv(file, usecols=['rerun','run','camcol','field'])
-    return objects[['rerun','run','camcol','field']].apply(lambda x: form_url(*x), axis=1).sum(), objects[['rerun','run','camcol','field']].apply(lambda x: getName3(*x), axis=1).sum()
 
-def allurlinseries1(file):
-    objects = pd.read_csv(file, usecols=['rerun','run','camcol','field', 'objid'])
-    return objects[['rerun','run','camcol','field']].apply(lambda x: form_url(*x), axis=1).sum(), objects[['rerun','run','camcol','field']].apply(lambda x: getName3(*x), axis=1).sum(), objects['objid'] 
-
-w = 16 # Window size (images of the size 2w*2w)
-for index, values in objects.iterrows(): 
-    band_arrays = []
-    rowc = int(round(values[[x for x in objects.columns if x.startswith('rowc_')]].mean()))
-    colc = int(round(values[[x for x in objects.columns if x.startswith('colc_')]].mean()))
-    for i in range(5):
-        filename = get_file_name(form_url(*values[['rerun', 'run', 'camcol', 'field']].astype('int'))[i])
-        file = fits.open(os.path.join('../data/images/', filename))
-        im = file[0].data
-        band_arrays.append(np.log(1 + -im.min() + 
-                                  im[max(0, rowc-w):min(rowc+w, im.shape[0]),
-                                     max(0,colc-w):min(colc+w, im.shape[1])]))
-        file.close()
-        del im
-    object_tensor = np.stack(band_arrays, axis=2)
-    del band_arrays
-    np.save(os.path.join('../data/object_images/', str(int(values['objid']))), object_tensor)
-    del object_tensor
-    
-  """
-"""
-
-def split_by_center(file_in_fit_format):
-    w = 16
-    file = fits.open(file_in_fit_format)
-    im = file[0].data
-    rowc = (im.shape[0]//2) 
-    colc = (im.shape[1]//2)
-    img = np.log(1-im.min() + im[rowc-w:rowc+w,colc-w:colc+w])
-    return img
-
-def split_normal(file_in_fit_format):
-    return split_by_center(file_in_fit_format)
-
-def get_final_fimage(directory, rerun, run, camcol, field):
-    final = []
-    file_names = getName3(rerun, run, camcol, field);
-    try:
-        for u in file_names:
-            final.append(split_by_center(u))
-    except:
-        return []
-    return np.stack(final, axis = 2)
-
-def dowload_split_save(url, file):
-    fit_dowload(url, file);
-    return split_normal(file)
-
-
-
-
-def dowload_final(file_in_csv_format, dir_all, dir_normales):
-    #./data/csvs/galaxies/1.csv'
-    urls, names, objid = allurlinseries1(str(file_in_csv_format));             
-    temp = []
-    pos = 1;
-    i = 0;
-    for v in range(len(urls)):
-        #./data/images/galaxies1/all
-        temp = dowload_split_save(urls[v], (str(dir_all)+'/{}').format(names[v]))
-        if (pos == 5):
-            #/data/images/galaxies1/normales
-            np.save((str(dir_normales)+'/{}').format( objid[i]), np.stack(temp,axis=2))
-            temp = []
-            i = i + 1
-        print(v)
-
-"""
         
 def save_preprocessing_images(data_path, dir_save, w, verbose = 0):
     #w = 16 # Window size (images of the size 2w*2w)
-    data_path = str(data_path); dir_save = str(dir_save); w = int(w);
+    data_path = str(data_path)
+    dir_save = str(dir_save); w = int(w)
     objects = pd.read_csv(data_path)
     ob = 1
-    for index, values in objects.iterrows(): 
-        band_arrays = []
+    filename = os.path.join(dir_save,'file.fits')
+    print('starting ......')
+    for index, values in objects.iterrows():
+        #try:
         rowc = int(round(values[[x for x in objects.columns if x.startswith('rowc_')]].mean()))
         colc = int(round(values[[x for x in objects.columns if x.startswith('colc_')]].mean()))
         all_urls_bands = form_url(*values[['rerun', 'run', 'camcol', 'field']].astype('int'));
+            
+        #print(all_urls_bands)
+        #print(len(all_urls_bands))
+            
         if (verbose == 1):
             print("Leading of object number : ", ob)
             ob = ob+1;
-        for i in range(5):
+        band_arrays = []
+        for i in range(len(all_urls_bands)):
+            #global band_arrays
             if (verbose == 1):
                 print('------| leading of band ',bands[i])
-            filename = 'file.fits'##get_file_name(all_urls_bands[i])
             fit_dowload(all_urls_bands[i], filename)
             file = fits.open(filename)
+            #print(file)
             im = file[0].data
-            band_arrays.append(np.log(1 + -im.min() + im[max(0, rowc-w):min(rowc+w, im.shape[0]), max(0,colc-w):min(colc+w,        im.shape[1])]))
+            band_arrays.append(np.log(1 + -im.min() + im[max(0, rowc-w):min(rowc+w, im.shape[0]), max(0,colc-w):min(colc+w, im.shape[1])]))
             file.close()
             del im
         object_tensor = np.stack(band_arrays, axis=2)
@@ -190,6 +107,8 @@ def save_preprocessing_images(data_path, dir_save, w, verbose = 0):
         # dir_save = '../data/images/galaxies1/all/'
         np.save(os.path.join(dir_save, str(int(values['objid']))), object_tensor)
         del object_tensor
+        #except :
+         #   print('incorrect object:')
     print('------------------------ dowloading ok ------------------------')
     
 def rmse_loss_keras(y_true, y_pred):
@@ -224,6 +143,29 @@ def model_nn(model, input_dim , n_hidden_layers, dropout=0, batch_normalization=
         model.compile(loss=rmse_loss_keras, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
     return model
 
+def model_nn2(model, input_dim , n_hidden_layers, dropout=0, batch_normalization=False, activation='relu', neurons_decay=0,starting_power=1,l2=0):
+    assert dropout >= 0 and dropout < 1
+    assert batch_normalization in {True, False}
+    for layer in range(n_hidden_layers):
+        #print(layer)
+        n_units = 2**(int(np.log2(input_dim)) + starting_power - layer*neurons_decay)
+        if n_units < 8:
+            n_units = 8
+        if layer == 0:
+            model = Dense(units=n_units, name='Dense_' + str(layer + 1), kernel_regularizer=keras.regularizers.l2(l2))(model)
+        else:
+            model = Dense(units=n_units, name='Dense_' + str(layer + 1), kernel_regularizer=keras.regularizers.l2(l2))(model)
+        if batch_normalization:
+            model = BatchNormalization(name='BatchNormalization_' + str(layer + 1))(model)
+        model = Activation(activation, name='Activation_' + str(layer + 1))(model)
+        if dropout > 0:
+            model = Dropout(dropout, name='Dropout_' + str(layer + 1))(model)
+    model = Dense(units=1, name='Dense_' + str(n_hidden_layers+1), kernel_regularizer=keras.regularizers.l2(l2))(model)
+    return model
+
+
+
+
 def save_model(model, file):
     model_json = model.to_json()
     with open(file, "w") as json_file:
@@ -243,7 +185,7 @@ def load_data(file_csv, dir_img):
     R = data['redshift']
     X = [] 
     Y = []
-    all_ = ''
+    #all_ = ''
     for v in range(len(O)):
         try:
             suffix = 'npy'
@@ -254,13 +196,13 @@ def load_data(file_csv, dir_img):
             Y.append(R[v])
         except :
             perte = perte + 1
-            print('Un objet de moins')
-            all_ = all_ + str(int(O[v])) +'\n'
+            #print('Un objet de moins')
+            #all_ = all_ + str(int(O[v])) +'\n'
     del O
     del R
     print('-------------- Data loading ok -----------')
     print('     perte = ', perte)
-    print(all_)
+    #print(all_)
     print('     number of images: ', len(X), ' class size :', len(Y))
     print(' -------- End -------')
     X = np.array(X)
@@ -268,7 +210,8 @@ def load_data(file_csv, dir_img):
     return X, Y   
     
 def get_train_test_valid_data_galaxy(file_csv, dir_img, test_size = 0.21, valid_size = 0.4):
-    X, Y = load_data(file_csv, dir_img)
+    #X, Y = load_data(file_csv, dir_img)
+    X, Y = get_all_in_folder(dir_img, file_csv)
     X_Train, X_Test, Y_Train, Y_Test =  train_test_split(X,Y, test_size = test_size)
     del X
     del Y
@@ -291,7 +234,16 @@ def compute_metrics(y_true, y_pred, clf_name):
     result.loc['|znorm| > 3std (%)'] = 100*np.sum(np.abs(delta_znorm) > 3*val)/y_true.shape[0]
     result.name = clf_name
     return result 
-    
+
+def plot_result(y_true, y_pred, x_name = "True redshift", y_name = "Predicted redshift",  title = "Fitness between predicted redshift and true redshift"):
+    pl.title(title)
+    #pl.plot(y_true, y_pred)
+    pl.scatter(y_true, y_pred)
+    pl.xlabel(x_name)
+    pl.ylabel(y_name)
+    pl.show()
+    pl.close()
+
 def plot_history(history, ymax=None):
     pl.figure(figsize=(15, 7))
     pl.plot(history.history['loss'])
@@ -333,5 +285,85 @@ def split_csv_data(file, result_dir, size_for_each=50):
         save(result_dir+'/'+str(nbr_split)+'.csv', data, type_save='w')
     file_.close();
     print('------------- spliting ok -----------------')
+    
+def get_all_in_folder(images_folder, file_csv):
+    
+    files = g.glob(images_folder)
+    
+    redshifts = []
+    data = []
+    
+    id_redshifts = pd.read_csv(file_csv, usecols=['objid','redshift'])
+    O = id_redshifts['objid'].tolist()
+    R = id_redshifts['redshift'].tolist()
+    nbr = 0
+    lost_list = []
+    del id_redshifts
+    
+    for name in files:
+        try:
+            fileName, fileExtension = os.path.splitext(os.path.basename(name))
+            fileName = int(fileName)
+            index = O.index(fileName)
+            redshifts.append(R[index])
+            try:
+                val = np.load(name)
+                if not val.shape == (32,32,5):
+                    raise Exception('','')
+                data.append(val)
+            except:
+                try:
+                    del redshifts[-1]
+                except:
+                    print('One problem')
+                nbr = nbr + 1
+                lost_list.append(fileName)
+        except:
+            nbr = nbr + 1
+            lost_list.append(fileName)
+    
+    del O
+    del R
+    
+    print('--------------------- End of loading ------------------------------')
+    print('                     |Loss: ', nbr)
+    #print(lost_list)
+    return np.array(data), np.array(redshifts)
+    
+def plot_hist(file_csv, name, bins = 3000, start =0, limit = 10):
+    X = []
+    for value in file_csv:
+        X = X + pd.read_csv(value, usecols=['redshift'])['redshift'].tolist()
+    pl.xlabel('Redshift')
+    pl.ylabel('Frequences')
+    pl.title(name)
+    #print(X[:40])
+    pl.hist(X, range = (start, limit), bins = bins, color = 'blue', edgecolor = 'red')
+    pl.show()
+
+def get_potentiels_dim (list_imgs, width = 32, height = 32, deep = 5):
+    rate_ = 0
+    pos_list = []
+    for i in range(width):
+        for j in range(height):
+            for k in range(deep):
+                total_value = len(list_imgs[:,i,j,k])
+                rate =  len(set(list_imgs[:,i,j,k]))/total_value
+                if rate > rate_:
+                    pos_list.append((i,j,k))
+                    rate_ = rate
+                elif rate == rate_:
+                    pos_list.append((i,j,k))
+    return pos_list, rate_
+    
+def get_urls(file, fi):
+    print('..file loiding ...')
+    objects = pd.read_csv(file, usecols=['objid','rerun', 'run', 'camcol', 'field'])
+    urls = objects[['rerun', 'run', 'camcol', 'field']].apply(lambda x: form_url(*x), axis=1).sum()
+    with open('download_images/'+fi, 'w') as f:
+        f.write('\n'.join(urls))
+    print('start ....')
+    os.system('wget -i download_images/{}  -P ../data2/images/'.format(fi));
+    print('end ....')
     
     
